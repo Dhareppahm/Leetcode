97fd620878dfdad64a079fdef8341489f252aee5
Sandeep Kulkarni <sandeep.kul88@gmail.com>
2015-10-22 20:05:23 -0700

    just try a simple problem to try leetcode

---
+++ src/leetcode/NimGame.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+public class NimGame {
+	
+	public static void main(String[] arg){
+		System.out.println(canWinNim(15));
+	}
+	
+	public static boolean canWinNim(int n) {
+        if(n % 4 == 0){
+            return false;
+        }else{
+            return true;
+        }
+    }
+}
