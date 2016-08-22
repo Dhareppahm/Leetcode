750ac85994226f40c68ac20f0ea8c7facfd856d5
Sandeep Kulkarni <sandeep.kul88@gmail.com>
2016-08-22 15:11:58 -0700

    Leetcode problems

--- src/leetcode/hard/tree/maximumsumpath/MaximumSumPath.java
+++ src/leetcode/hard/tree/maximumsumpath/MaximumSumPath.java
@@ -84,10 +84,10 @@ public class MaximumSumPath {
         BinaryTree bt = new BinaryTree();
 
         bt.root = new Node(-1);
-        /*bt.root.left = new Node(2);
+        bt.root.left = new Node(2);
         bt.root.right = new Node(-4);
         bt.root.left.left = new Node(2);
-        bt.root.left.right = new Node(-3);*/
+        bt.root.left.right = new Node(-3);
 
         System.out.println("Binary Tree: ");
         bt.displayInorder(bt.root);
